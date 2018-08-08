8ea7bea8e62ffdc1795286fba3acb18e98b4800e
Grigory Zhadko <gregzhadko@gmail.com>
2018-08-08 16:11:50 +0200

    Added first task solution

---
+++ Solution/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IsUnique
+{
+    public class Solution
+    {
+        public static bool SolutionWithDictionary(string input)
+        {
+            var dict = new Dictionary<char, bool>();
+
+            foreach (var c in input)
+            {
+                if (dict.ContainsKey(c))
+                {
+                    return false;
+                }
+                
+                dict.Add(c, true);
+            }
+            
+            return true;
+        }
+
+        public static bool SolutionWithoutSdditionalDataStructure(string input)
+        {
+            var sorted = input.OrderBy(c => c).ToList();
+            for (int i = 0; i < sorted.Count - 1; i++)
+            {
+                if (sorted[i] == sorted[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ SolutionTests/Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using IsUnique;
+using NUnit.Framework;
+
+namespace SolutionTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var actual = Solution.SolutionWithDictionary("tes");
+            Assert.AreEqual(true, actual);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            var actual = Solution.SolutionWithDictionary("test");
+            Assert.AreEqual(false, actual);
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            var actual = Solution.SolutionWithDictionary("tes1234567");
+            Assert.AreEqual(true, actual);
+        }
+        
+        [Test]
+        public void Test12()
+        {
+            var actual = Solution.SolutionWithoutSdditionalDataStructure("tes");
+            Assert.AreEqual(true, actual);
+        }
+        
+        [Test]
+        public void Test22()
+        {
+            var actual = Solution.SolutionWithoutSdditionalDataStructure("test");
+            Assert.AreEqual(false, actual);
+        }
+        
+        [Test]
+        public void Test32()
+        {
+            var actual = Solution.SolutionWithoutSdditionalDataStructure("tes1234567");
+            Assert.AreEqual(true, actual);
+        }
+    }
+}
