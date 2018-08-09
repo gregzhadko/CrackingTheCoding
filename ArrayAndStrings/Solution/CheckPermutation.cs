704b6e4c680596ef7899cc9c102e3cf368b4b91d
Grigory Zhadko <gregzhadko@gmail.com>
2018-08-09 13:24:40 +0200

    Check Permutation

---
+++ ArrayAndStrings/Solution/CheckPermutation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Solution
+{
+    public static class CheckPermutation
+    {
+        public static bool IsPermutation(string original, string permutation)
+        {
+            if (original.Length != permutation.Length)
+            {
+                return false;
+            }
+            
+            var dictionary = new Dictionary<char, int>();
+            foreach (var key in original)
+            {
+                if (!dictionary.ContainsKey(key))
+                {
+                    dictionary.Add(key, 1);
+                }
+                else
+                {
+                    dictionary[key]++;
+                }
+            }
+
+            foreach (var key in permutation)
+            {
+                if (!dictionary.ContainsKey(key))
+                {
+                    return false;
+                }
+
+                dictionary[key]--;
+
+                if (dictionary[key] < 0)
+                {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ ArrayAndStrings/SolutionTests/CheckPermutationTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Solution;
+
+namespace SolutionTests
+{
+    [TestFixture]
+    public sealed class CheckPermutationTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var actual = CheckPermutation.IsPermutation("test", "estt");
+            Assert.IsTrue(actual);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            var actual = CheckPermutation.IsPermutation("test", "tes");
+            Assert.IsFalse(actual);
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            var actual = CheckPermutation.IsPermutation("test", "tess");
+            Assert.IsFalse(actual);
+        }
+        
+        [Test]
+        public void Test4()
+        {
+            var actual = CheckPermutation.IsPermutation("test", "tset");
+            Assert.IsTrue(actual);
+        }
+        
+        [Test]
+        public void Test5()
+        {
+            var actual = CheckPermutation.IsPermutation("test111111", "111111tset");
+            Assert.IsTrue(actual);
+        }
+        
+        [Test]
+        public void Test6()
+        {
+            var actual = CheckPermutation.IsPermutation("test111111", "111112tset");
+            Assert.IsFalse(actual);
+        }
+    }
+}
