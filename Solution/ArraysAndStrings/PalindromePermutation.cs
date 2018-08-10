088e3506b8b3e3a84f9cb95c88591e1da08d7d62
Grigory Zhadko <gregzhadko@gmail.com>
2018-08-10 17:49:52 +0200

    Palindrome Permutation task

---
+++ Solution/ArraysAndStrings/PalindromePermutation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution.ArraysAndStrings
+{
+    public class PalindromePermutation
+    {
+        public static bool IsPalindromePermutation(string input)
+        {
+            var dict = new Dictionary<char, int>();
+
+            var realSize = 0;
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                var c = Char.ToLower(input[i]);
+
+                if (c == ' ')
+                {
+                    continue;
+                }
+
+                realSize++;
+                if (!dict.ContainsKey(c))
+                {
+                    dict.Add(c, 1);
+                }
+                else
+                {
+                    dict[c]++;
+                }
+            }
+
+            var odd = realSize % 2 == 1;
+
+            var usedMiddle = false;
+            foreach (var key in dict.Keys)
+            {
+                if (dict[key] % 2 != 1) continue;
+
+                if (!odd) return false;
+                
+                if (usedMiddle) return false;
+
+                usedMiddle = true;
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ SolutionTests/ArraysAndStringsTests/PalindromePermutationTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Solution.ArraysAndStrings;
+
+namespace SolutionTests.ArraysAndStringsTests
+{
+    [TestFixture]
+    public sealed class PalindromePermutationTests
+    {
+        [Test]
+        [TestCase("А роза упала на лапу Азора")]
+        [TestCase("Tact Coa")]
+        [TestCase("Tact Coaaa")]
+        [TestCase("TactCoa")]
+        [TestCase("a")]
+        [TestCase("aa")]
+        public void Test_PalindromePermutation_Success(string input)
+        {
+            var actual = PalindromePermutation.IsPalindromePermutation(input);
+            Assert.IsTrue(actual);
+        }
+        
+        [Test]
+        [TestCase("А роза упала на лапу Азора1")]
+        [TestCase("TactCCoa")]
+        [TestCase("ac")]
+        public void Test_NotPalindromePermutation_Success(string input)
+        {
+            var actual = PalindromePermutation.IsPalindromePermutation(input);
+            Assert.IsFalse(actual);
+        }
+    }
+}
