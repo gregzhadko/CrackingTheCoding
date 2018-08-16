39c015b994b99ede403fea0da47abd9d986ca081
Grigory Zhadko <gregzhadko@gmail.com>
2018-08-16 18:01:10 +0200

    One Way task is implemented

---
+++ Solution/ArraysAndStrings/OneWay.cs
@@ -0,0 +1,73 @@
+﻿namespace Solution.ArraysAndStrings
+{
+    public class OneWay
+    {
+        public bool IsOneEdit(string original, string resulted)
+        {
+            if (original.Length == resulted.Length)
+            {
+                return IsOneSymbolChanged(original, resulted);
+            }
+
+            if (resulted.Length - original.Length == 1)
+            {
+                return IsOneSymbolAdded(original, resulted);
+            }
+
+            if (original.Length - resulted.Length == 1)
+            {
+                return IsOneSymbolAdded(resulted, original);
+            }
+
+            return false;
+        }
+
+        private static bool IsOneSymbolChanged(string original, string resulted)
+        {
+            bool wasChanged = false;
+
+            for (int i = 0; i < original.Length; i++)
+            {
+                if (original[i] == resulted[i]) continue;
+
+                if (wasChanged)
+                {
+                    return false;
+                }
+
+                wasChanged = true;
+            }
+
+            return wasChanged;
+        }
+
+        private bool IsOneSymbolAdded(string smaller, string bigger)
+        {
+            bool wasDifferentFound = false;
+            
+            var j = 0;
+            for (int i = 0; i < bigger.Length; i++, j++)
+            {
+                if (j == smaller.Length)
+                {
+                    return !wasDifferentFound;
+                }
+                
+                if (smaller[j] == bigger[i])
+                {
+                    continue;
+                }
+
+                if (wasDifferentFound)
+                {
+                    return false;
+                }
+
+                wasDifferentFound = true;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ SolutionTests/ArraysAndStringsTests/OneWayTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using Solution.ArraysAndStrings;
+
+namespace SolutionTests.ArraysAndStringsTests
+{
+    [TestFixture]
+    public sealed class OneWayTests
+    {
+        [Test]
+        [TestCase("pale", "ple")]
+        [TestCase("pales", "pale")]
+        public void RemoveEditApplied_Success(string original, string resulted)
+        {
+            var oneWay = new OneWay();
+            Assert.IsTrue(oneWay.IsOneEdit(original, resulted));
+        }
+        
+        [Test]
+        [TestCase("ple", "pale")]
+        [TestCase("pale", "pales")]
+        public void AddEditApplied_Success(string original, string resulted)
+        {
+            var oneWay = new OneWay();
+            Assert.IsTrue(oneWay.IsOneEdit(original, resulted));
+        }
+        
+        [Test]
+        [TestCase("pale", "bale")]
+        public void ReplaceEditApplied_Success(string original, string resulted)
+        {
+            var oneWay = new OneWay();
+            Assert.IsTrue(oneWay.IsOneEdit(original, resulted));
+        }
+        
+        [Test]
+        [TestCase("pale", "bake")]
+        [TestCase("pale", "apaler")]
+        [TestCase("pale", "palis")]
+        public void SeveralEditApplies_Failure(string original, string resulted)
+        {
+            var oneWay = new OneWay();
+            Assert.IsFalse(oneWay.IsOneEdit(original, resulted));
+        }
+        
+        [Test]
+        [TestCase("pale", "pale")]
+        public void NoChanges_Failure(string original, string resulted)
+        {
+            var oneWay = new OneWay();
+            Assert.IsFalse(oneWay.IsOneEdit(original, resulted));
+        }
+    }
+}
