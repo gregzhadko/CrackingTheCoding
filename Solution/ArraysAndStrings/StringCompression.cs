e76eaf9e96a3ff01e785b3d8e4abb782754119f0
Grigory Zhadko <gregzhadko@gmail.com>
2018-08-21 17:35:39 +0200

    StringCompression task

---
+++ Solution/ArraysAndStrings/StringCompression.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace Solution.ArraysAndStrings
+{
+    public sealed class StringCompression
+    {
+        public string Compress(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+
+            var formatted = input.ToLowerInvariant();
+
+            var stringBuilder = new StringBuilder();
+
+            var currentElement = formatted[0];
+            stringBuilder.Append(currentElement);
+            var count = 1;
+            bool crypted = false;
+            for (int i = 1; i < formatted.Length; i++)
+            {
+                if (formatted[i] != currentElement)
+                {
+                    stringBuilder.Append(count);
+                    currentElement = formatted[i];
+                    stringBuilder.Append(currentElement);
+                    count = 1;
+                }
+                else
+                {
+                    count++;
+                    crypted = true;
+                }
+            }
+
+            stringBuilder.Append(count);
+
+            return crypted ? stringBuilder.ToString() : formatted;
+        }
+    }
+}
---
+++ SolutionTests/ArraysAndStringsTests/StringCompressionTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Solution.ArraysAndStrings;
+
+namespace SolutionTests.ArraysAndStringsTests
+{
+    [TestFixture]
+    public sealed class StringCompressionTests
+    {
+        [Test]
+        [TestCase("aaa", "a3")]
+        [TestCase("bBBbbB", "b6")]
+        public void OneLetterCompression(string input, string expected)
+        {
+            var actual = new StringCompression().Compress(input);
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        [TestCase("aaabbbccd", "a3b3c2d1")]
+        [TestCase("abcddd", "a1b1c1d3")]
+        public void ManyLettersCompression(string input, string expected)
+        {
+            var actual = new StringCompression().Compress(input);
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        [TestCase("a", "a")]
+        [TestCase("abc", "abc")]
+        public void NoCompression(string input, string expected)
+        {
+            var actual = new StringCompression().Compress(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
